b42f0f236bae4e65c14e23ebfb1cc649451290aa
Dexter St-Pierre <Dexter.stpierre@gmail.comgit config --global.namegit config --global user.namegit config --global user.email Dexter.stpierre@gmail.com>
2017-08-07 12:13:45 -0500

    started on post route for new trip

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -5,22 +5,17 @@ myApp.config(function($routeProvider, $locationProvider) {
   $locationProvider.hashPrefix('');
   console.log('myApp -- config')
   $routeProvider
-    .when('/home', {
-      templateUrl: '/views/templates/home.html',
+    .when('/login', {
+      templateUrl: '/views/templates/login.html',
       controller: 'LoginController as lc',
     })
     .when('/register', {
       templateUrl: '/views/templates/register.html',
       controller: 'LoginController as lc'
     })
-    .when('/user', {
-      templateUrl: '/views/templates/user.html',
-      controller: 'UserController as uc',
-      resolve: {
-        getuser : function(UserService){
-          return UserService.getuser();
-        }
-      }
+    .when('/home', {
+      templateUrl: '/views/templates/home.html',
+      controller: 'NewTripController as ntc',
     })
     .when('/info', {
       templateUrl: '/views/templates/info.html',

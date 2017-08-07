3f71dee14b4a3944f42888ad60fb68b1df0b68e3
Dexter St-Pierre <Dexter.stpierre@gmail.comgit config --global.namegit config --global user.namegit config --global user.email Dexter.stpierre@gmail.com>
2017-08-07 13:36:42 -0500

    accesses and api-requests

--- server/routes/trips.router.js
+++ server/routes/trips.router.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 var path = require('path');
+var requests = require('../modules/api.requests.js');
 
 router.post('/newtrip', function(req, res) {
   console.log(req.body);
-  
-  res.sendStatus(200);
+  requests.newTrip(req.body);
 })
 
 module.exports = router;

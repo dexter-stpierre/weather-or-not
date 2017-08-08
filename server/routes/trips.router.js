b11580d2de809751821fdb36a770476ca72a1932
Dexter St-Pierre <Dexter.stpierre@gmail.comgit config --global.namegit config --global user.namegit config --global user.email Dexter.stpierre@gmail.com>
2017-08-08 08:57:56 -0500

    setinterval setup

--- server/routes/trips.router.js
+++ server/routes/trips.router.js
@@ -3,14 +3,40 @@ var router = express.Router();
 var passport = require('passport');
 var path = require('path');
 var requests = require('../modules/api.requests.js');
+var when = require('when');
 
 router.post('/newtrip', function(req, res) {
+  var loop = 0;
   var trip = {};
   console.log(req.body);
-  trip.route = requests.newTrip(req.body);
-  if (trip.route != undefined) {
-    console.log(trip);
-  }
-})
+  newTrip = req.body;
+  // trip.route = requests.newTrip(req.body);
+  // if (trip.route != undefined) {
+  //   console.log(trip);
+  // }
+  requests.newTrip(newTrip);
+  var checkApi = setInterval(function(){
+    loop ++;
+    console.log('interval');
+    if(requests.route != undefined){
+      if(requests.distancePolyline != undefined){
+        if(requests.timePolyline != undefined){
+          console.log('api request complete');
+          clearInterval(checkApi);
+        }
+      }
+    } //else if (requests.route) {
+
+    //}
+    else if(loops > 30){
+      console.log("api failure");
+      clearInterval(checkApi);
+    }
+  }, 1000)
+});
+
+function logThis(){
+  console.log('this should be last');
+}
 
 module.exports = router;

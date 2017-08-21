09fa2520cf7ec1df04a7fb3a983e64375f22cc45
Dexter St-Pierre <Dexter.stpierre@gmail.comgit config --global.namegit config --global user.namegit config --global user.email Dexter.stpierre@gmail.com>
2017-08-20 19:43:47 -0500

    hid api keys

--- server/api-keys/apixu.js
+++
@@ -1,6 +0,0 @@
-var key = {
-  key: process.env.WEATHER_KEY || 'aea308ab51d04f49b28150450171108'
-}
-
-//export key
-module.exports = key
--- server/api-keys/open-route-services-api.js
+++
@@ -1,6 +0,0 @@
-var key = {
-  key: process.env.OPEN_ROUTE_KEY || '58d904a497c67e00015b45fcfb4bd974e8b84dfd4e88dd0bbc08914d'
-}
-
-//export key
-module.exports = key;
